e7b5cf51db6776b8466b52a1e48a54f69d2f96d8
AustinHan27 <79199541@qq.com>
2022-11-14 16:09:50 +0800

    [update] 网关及后台管理系统

--- mall-product/src/main/java/cn/hyy/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/cn/hyy/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,14 @@
 package cn.hyy.mall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +18,8 @@ import cn.hyy.common.utils.Query;
 import cn.hyy.mall.product.dao.CategoryDao;
 import cn.hyy.mall.product.entity.CategoryEntity;
 import cn.hyy.mall.product.service.CategoryService;
+
+import javax.xml.ws.Action;
 
 
 @Service("categoryService")
@@ -26,4 +35,26 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> categoryEntities = this.baseMapper.selectList(null);
+        return categoryEntities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map(menu -> {
+                    menu.setChildren(getChildrens(menu, categoryEntities));
+                    return menu;
+                }).sorted(Comparator.comparingInt(CategoryEntity::getSort))
+                .collect(Collectors.toList());
+    }
+
+    private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> list) {
+        return list.stream().filter(entity -> Objects.equals(entity.getParentCid(), root.getCatId()))
+                .map(category -> {
+                    category.setChildren(getChildrens(category, list));
+                    return category;
+
+                })
+                .sorted(Comparator.comparingInt(CategoryEntity::getSort))
+                .collect(Collectors.toList());
+
+    }
 }
